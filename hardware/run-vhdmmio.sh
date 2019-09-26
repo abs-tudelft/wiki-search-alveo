099b0ab77df7488816cb76da25de54cc5cfd33f8
Jeroen van Straten <jeroen.van.straten@gmail.com>
2019-09-26 15:13:53 +0200

    Add Alveo/SDAccel-compatible register file

--- hardware/run-vhdmmio.sh
+++ hardware/run-vhdmmio.sh
@@ -1,1 +1,6 @@
-vhdmmio -P vhdl -V vhdl -H mmio-doc
+if [ ! -f $1.mmio.yaml ]; then
+  echo "specify name of MMIO description file to generate ('fletchgen' or 'alveo')"
+  exit 1
+fi
+
+vhdmmio $1.mmio.yaml -P vhdl -V vhdl -H mmio-doc
