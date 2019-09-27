587f85ccc0a7672632391e14d783d55c0ed4bcf3
Matthijs Brobbel <m1brobbel@gmail.com>
2019-09-27 11:01:42 +0200

    Host application and Makefile patches to add Arrow

--- alveo/src/host.cpp
+++ alveo/src/host.cpp
@@ -29,32 +29,51 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "xcl2.hpp"
 #include <vector>
 
+#include <arrow/api.h>
+#include <arrow/io/api.h>
+#include <arrow/ipc/api.h>
+
 #define DATA_SIZE 256
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
+    if (argc != 3) {
+        std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << " <RECORDBATCH File> " << std::endl;
         return EXIT_FAILURE;
     }
 
     std::string binaryFile = argv[1];
+    std::string recordbatchFile = argv[2];
+
+    arrow::Status status;
+    std::shared_ptr<arrow::io::ReadableFile> file;
+    std::shared_ptr<arrow::ipc::RecordBatchFileReader> reader;
+    std::shared_ptr<arrow::RecordBatch> batch;
+
+    status = arrow::io::ReadableFile::Open(recordbatchFile, &file);
+    status = arrow::ipc::RecordBatchFileReader::Open(file, &reader);
+
+    std::cout << reader->num_record_batches() << std::endl;
+    status = reader->ReadRecordBatch(0, &batch);
+    std::cout << batch->num_rows() << std::endl;
+    std::cout << reader->schema()->ToString() << std::endl;
+
+    std::shared_ptr<arrow::ArrayData> titleData = batch->column_data(0);
+    std::shared_ptr<arrow::ArrayData> textData = batch->column_data(1);
+
+    std::cout << titleData->length << std::endl;
+    std::cout << textData->length << std::endl;
+
+    std::cout << titleData->buffers.size() << std::endl;
+ //   std::cout << (char*)(titleData->buffers[2]->data()) << std::endl;
+ //   std::cout << (char*)(textData->buffers[2]->data()) << std::endl;
 
     cl_int err;
-    auto size = DATA_SIZE;
-    //Allocate Memory in Host Memory
-    auto vector_size_bytes = sizeof(int) * size;
-    std::vector<int, aligned_allocator<int>> source_input1(size);
-    std::vector<int, aligned_allocator<int>> source_input2(size);
-    std::vector<int, aligned_allocator<int>> source_hw_results(size);
-    std::vector<int, aligned_allocator<int>> source_sw_results(size);
 
-    // Create the test data and Software Result
-    for (int i = 0; i < size; i++) {
-        source_input1[i] = i;
-        source_input2[i] = i;
-        source_sw_results[i] = source_input1[i] + source_input2[i];
-        source_hw_results[i] = 0;
-    }
+    // TODO: used to pass valid pointers for the result table; remove me
+    int dummy = 0;
+
+    int first_idx = 0;
+    int last_idx = 1; // TODO
 
     //OPENCL HOST CODE AREA START
     //Create Program and Kernel
@@ -67,69 +86,136 @@ int main(int argc, char **argv) {
         cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
     auto device_name = device.getInfo<CL_DEVICE_NAME>();
 
-   auto fileBuf = xcl::read_binary_file(binaryFile);
-   cl::Program::Binaries bins{{fileBuf.data(), fileBuf.size()}};
+    auto fileBuf = xcl::read_binary_file(binaryFile);
+    cl::Program::Binaries bins{{fileBuf.data(), fileBuf.size()}};
     devices.resize(1);
     OCL_CHECK(err, cl::Program program(context, devices, bins, NULL, &err));
+    
+    #if 0
     OCL_CHECK(err, cl::Kernel krnl_word_match(program, "krnl_word_match_rtl", &err));
 
-    //Allocate Buffer in Global Memory
-    OCL_CHECK(err,
-              cl::Buffer buffer_r1(context,
-                                   CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-                                   vector_size_bytes,
-                                   source_input1.data(),
-                                   &err));
-    OCL_CHECK(err,
-              cl::Buffer buffer_r2(context,
-                                   CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-                                   vector_size_bytes,
-                                   source_input2.data(),
-                                   &err));
-    OCL_CHECK(err,
-              cl::Buffer buffer_w(context,
-                                  CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
-                                  vector_size_bytes,
-                                  source_hw_results.data(),
-                                  &err));
+    // Allocate input buffers.
+    OCL_CHECK(err, cl::Buffer buffer_title_offs(
+        context,
+        CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+        titleData->buffers[1]->size(),
+        titleData->buffers[1]->mutable_data(),
+        &err));
 
-    //Set the Kernel Arguments
-    OCL_CHECK(err, err = krnl_word_match.setArg(0, buffer_r1));
-    OCL_CHECK(err, err = krnl_word_match.setArg(1, buffer_r2));
-    OCL_CHECK(err, err = krnl_word_match.setArg(2, buffer_w));
-    OCL_CHECK(err, err = krnl_word_match.setArg(3, size));
+    OCL_CHECK(err, cl::Buffer buffer_title_val(
+        context,
+        CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+        titleData->buffers[2]->size(),
+        titleData->buffers[2]->mutable_data(),
+        &err));
+
+    OCL_CHECK(err, cl::Buffer buffer_text_offs(
+        context,
+        CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+        textData->buffers[1]->size(),
+        textData->buffers[1]->mutable_data(),
+        &err));
+
+    OCL_CHECK(err, cl::Buffer buffer_text_val(
+        context,
+        CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+        textData->buffers[2]->size(),
+        textData->buffers[2]->mutable_data(),
+        &err));
+
+    OCL_CHECK(err, err = krnl_word_match.setArg(0, buffer_title_offs));
+    OCL_CHECK(err, err = krnl_word_match.setArg(1, buffer_title_val));
+    OCL_CHECK(err, err = krnl_word_match.setArg(2, buffer_text_offs));
+    OCL_CHECK(err, err = krnl_word_match.setArg(3, buffer_text_val));
+    OCL_CHECK(err, err = krnl_word_match.setArg(4, first_idx));
+    OCL_CHECK(err, err = krnl_word_match.setArg(5, last_idx));
+
+    // Allocate result buffers.
+    OCL_CHECK(err, cl::Buffer buffer_res_title_offs(
+        context,
+        CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+        4,
+        &dummy,
+        &err));
+
+    OCL_CHECK(err, cl::Buffer buffer_res_title_val(
+        context,
+        CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+        4,
+        &dummy,
+        &err));
+
+    OCL_CHECK(err, cl::Buffer buffer_res_match(
+        context,
+        CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+        4,
+        &dummy,
+        &err));
+
+    uint64_t res_stats = 0xDEADC0DEDEADC0DEull;
+
+    OCL_CHECK(err, cl::Buffer buffer_res_stats(
+        context,
+        CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+        sizeof(res_stats),
+        &res_stats,
+        &err));
+
+    OCL_CHECK(err, err = krnl_word_match.setArg(6, buffer_res_title_offs));
+    OCL_CHECK(err, err = krnl_word_match.setArg(7, buffer_res_title_val));
+    OCL_CHECK(err, err = krnl_word_match.setArg(8, buffer_res_match));
+    OCL_CHECK(err, err = krnl_word_match.setArg(9, 0 /* TODO_NUM_RESULTS */));
+    OCL_CHECK(err, err = krnl_word_match.setArg(10, buffer_res_stats));
+
+    // Configure the search string.
+    std::string pattern = "April";
+    bool whole_words = true;
+    int min_matches = 1;
+
+    uint32_t pattern_data[8];
+    char *pattern_chars = reinterpret_cast<char*>(pattern_data);
+    int first = 32 - pattern.length();
+    if (first < 0) {
+        std::cerr << "Search pattern is too long" << std::endl;
+        return EXIT_FAILURE;
+    }
+    for (int i = 0; i < first; i++) {
+        pattern_chars[i] = 0;
+    }
+    for (int i = first; i < 32; i++) {
+        pattern_chars[i] = pattern[i - first];
+    }
+    uint32_t search_config = static_cast<uint32_t>(first);
+    if (whole_words) search_config |= 1 << 8;
+    search_config |= static_cast<uint32_t>(min_matches) << 16;
+
+    OCL_CHECK(err, err = krnl_word_match.setArg(11, search_config));
+    for (int i = 0; i < 8; i++) {
+        OCL_CHECK(err, err = krnl_word_match.setArg(12 + i, pattern_data[i]));
+    }
 
     //Copy input data to device global memory
-    OCL_CHECK(err,
-              err = q.enqueueMigrateMemObjects({buffer_r1, buffer_r2},
-                                               0 /* 0 means from host*/));
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects(
+        {buffer_title_offs, buffer_title_val, buffer_text_offs, buffer_text_val},
+        0 /* 0 means from host*/));
 
     //Launch the Kernel
     OCL_CHECK(err, err = q.enqueueTask(krnl_word_match));
 
     //Copy Result from Device Global Memory to Host Local Memory
-    OCL_CHECK(err,
-              err = q.enqueueMigrateMemObjects({buffer_w},
-                                               CL_MIGRATE_MEM_OBJECT_HOST));
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects(
+        {buffer_res_title_offs, buffer_res_title_val, buffer_res_match, buffer_res_stats},
+        CL_MIGRATE_MEM_OBJECT_HOST));
+
     OCL_CHECK(err, err = q.finish());
 
     //OPENCL HOST CODE AREA END
 
     // Compare the results of the Device to the simulation
-    int match = 0;
-    for (int i = 0; i < size; i++) {
-        if (source_hw_results[i] != source_sw_results[i]) {
-            std::cout << "Error: Result mismatch" << std::endl;
-            std::cout << "i = " << i
-                      << " Software result = " << source_sw_results[i]
-                      << " Device result = " << source_hw_results[i]
-                      << std::endl;
-            match = 1;
-            break;
-        }
-    }
+    printf("res_stats: 0x%016llX\n", res_stats);
 
+    #endif
 
-    std::cout << "TEST " << (match ? "FAILED" : "PASSED") << std::endl;
-    return (match ? EXIT_FAILURE : EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
+
