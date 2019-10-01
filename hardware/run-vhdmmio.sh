64251367ee2eb37d59675632b859c644dd90eb00
Jeroen van Straten <jeroen.van.straten@gmail.com>
2019-10-01 16:56:36 +0200

    Separate alveo and fletchgen mmio sources

--- hardware/run-vhdmmio.sh
+++ hardware/run-vhdmmio.sh
@@ -1,6 +1,1 @@
-if [ ! -f $1.mmio.yaml ]; then
-  echo "specify name of MMIO description file to generate ('fletchgen' or 'alveo')"
-  exit 1
-fi
-
-vhdmmio $1.mmio.yaml -P vhdl -V vhdl -H mmio-doc
+vhdmmio -P vhdl -V @ -H mmio-doc
