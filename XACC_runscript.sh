f84124405c2ef546f1b6c832120d4d19108c4d8c
Joost Hoozemans <jhoozemans@alveo2a.ethz.ch>
2021-06-22 11:21:54 +0200

    Runscript now checking whether Rust is installed

--- XACC_runscript.sh
+++ XACC_runscript.sh
@@ -1,6 +1,13 @@
+
+if [ ! -d $HOME/.cargo ]; then
+  echo "Cannot find Rust, please run the build script first."
+  exit -1
+fi
+source $HOME/.cargo/env
+
 # Start the application
 echo "Running the application..."
 cd server && \
 	LD_LIBRARY_PATH=../alveo:../work/arrow/install/lib:../work/snappy/install/lib \
-	cargo run ../data/simplewiki-rechunked ../alveo/xclbin/word_match
+	cargo run ../data/simplewiki-rechunked ../alveo/vitis-2019.2/xclbin/word_match
 
