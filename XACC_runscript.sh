27fe7e1ef699c41c034d79790c3382369cff59ed
Joost Hoozemans <joosthooz@msn.com>
2021-06-25 10:18:37 +0200

    Apply suggestions from code review
    Co-authored-by: Matthijs Brobbel <m1brobbel@gmail.com>

--- XACC_runscript.sh
+++ XACC_runscript.sh
@@ -1,13 +1,6 @@
 
-if [ ! -d $HOME/.cargo ]; then
-  echo "Cannot find Rust, please run the build script first."
-  exit -1
-fi
-source $HOME/.cargo/env
-
 # Start the application
 echo "Running the application..."
 cd server && \
 	LD_LIBRARY_PATH=../alveo:../work/arrow/install/lib:../work/snappy/install/lib \
-	cargo run ../data/simplewiki-rechunked ../alveo/vitis-2019.2/xclbin/word_match
-
+	./target/release/server ../data/simplewiki-rechunked ../alveo/vitis-2019.2/xclbin/word_match
